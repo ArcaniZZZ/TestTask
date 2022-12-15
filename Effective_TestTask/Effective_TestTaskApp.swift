ef96a6c6ad64f58054619ead9949e2fac88e5d46
Alex <a.zubach@finch-melrose.com>
2022-12-15 13:23:51 +0400

    Initial Commit

---
+++ Effective_TestTask/Effective_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Effective_TestTaskApp.swift
+//  Effective_TestTask
+//
+//  Created by Arcani on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Effective_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
